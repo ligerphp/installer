0cce8f3ecf924831f07da6455b04862a11de31ca
ligerphp <johnsonmessilo19@gmail.com>
2019-05-14 05:08:43 -0700

    ready for local test, no download

--- src/NewCommand.php
+++ src/NewCommand.php
@@ -132,7 +132,7 @@ class NewCommand extends Command
                 break;
         }
 
-        $response = (new Client)->get('https://github.com/ligerphp/liger/archive'.$filename);
+        $response = (new Client)->get('http://localhost/liger/'.$filename);
 
         file_put_contents($zipFile, $response->getBody());
 
@@ -186,10 +186,10 @@ class NewCommand extends Command
         $filesystem = new Filesystem;
 
         try {
-            $filesystem->chmod($appDirectory.DIRECTORY_SEPARATOR.'bootstrap/cache', 0755, 0000, true);
+            $filesystem->chmod($appDirectory.DIRECTORY_SEPARATOR.'bootstrap', 0755, 0000, true);
             $filesystem->chmod($appDirectory.DIRECTORY_SEPARATOR.'database', 0755, 0000, true);
         } catch (IOExceptionInterface $e) {
-            $output->writeln('<comment>You should verify that the "database" and "bootstrap/cache" directories are writable.</comment>');
+            $output->writeln('<comment>You should verify that the "database" and "bootstrap" directories are writable.</comment>');
         }
 
         return $this;
